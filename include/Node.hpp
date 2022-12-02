668fa40843b3acca4595cb54c7144516c7e3c41b
sassy23 <sundaram.seivur@gmail.com>
2022-12-02 18:42:15 -0500

    Deciding code structure

---
+++ include/Node.hpp
@@ -0,0 +1,12 @@
+//create Node
+class Node
+{
+    public:
+    
+    Node();
+    
+    bool isVisited;
+    //parent
+    //neighnors
+
+};
--- src/coveragePlanner.cpp
+++ src/coveragePlanner.cpp
@@ -7,16 +7,23 @@ class Event
 
 };
 
-//create Node
-class Node
-{
-    public:
-    
-    Node();
-    
-    bool isVisited;
-    //parent
-    //neighnors
+//take obstacle center, height, width, and angle as input
+//rotate the obstacles as necessary
 
+//trapezoidal decomposition
+//get events from polygon - first send the outer boundry (map)
+
+class point2D
+{
+    private:
+    int x;
+    int y;
 };
 
+std::vector<point2D> map_boundary;
+std::vector<std::vector<point2D>> obstacles;
+
+void get_event_type(std::vector<point2D> polygon)
+{
+    
+}
