d1f6d44f0716b510b81bf0e9d417bd7dc3ae5b4f
sassy23 <sundaram.seivur@gmail.com>
2022-12-01 22:47:36 -0500

    Created Coverage PLanner file

---
+++ src/coveragePlanner.cpp
@@ -0,0 +1,22 @@
+#include "coveragePlanner.hpp"
+
+//create events
+class Event
+{
+    //will take the position of the obstacles and some other things maybe
+
+};
+
+//create Node
+class Node
+{
+    public:
+    
+    Node();
+    
+    bool isVisited;
+    //parent
+    //neighnors
+
+};
+
